0890fefb4c88bb2a4a7ce4701551a742db01eca1
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-22 15:46:39 -0700

    Update load_images.py

--- src/load_images.py
+++ src/load_images.py
@@ -18,3 +18,21 @@ def pickle_images_labels():
 	return images_labels
 
 images_labels = pickle_images_labels()
+images_labels = shuffle(shuffle(shuffle(shuffle(images_labels))))
+images, labels = zip(*images_labels)
+print("Length of images_labels", len(images_labels))
+
+train_images = images[:int(5/6*len(images))]
+print("Length of train_images", len(train_images))
+with open("train_images", "wb") as f:
+	pickle.dump(train_images, f)
+del train_images
+
+train_labels = labels[:int(5/6*len(labels))]
+print("Length of train_labels", len(train_labels))
+with open("train_labels", "wb") as f:
+	pickle.dump(train_labels, f)
+del train_labels
+
+test_images = images[int(5/6*len(images)):int(11/12*len(images))]
+print("Length of test_images", len(test_images))
